4a0288d35cf0ff2bdde6e31cace62630f6d4a63f
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-05-17 17:57:25 -0300

    done with api gateway

--- ProductService/src/main/java/com/gianvittorio/estore/ProductService/web/controller/ProductController.java
+++ ProductService/src/main/java/com/gianvittorio/estore/ProductService/web/controller/ProductController.java
@@ -1,5 +1,8 @@
 package com.gianvittorio.estore.ProductService.web.controller;
 
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,23 +10,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
+    public String getPort() {
+        return environment.getProperty("local.server.port");
+    }
+
     @PostMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String createProduct() {
-        return "http post is handled";
+        return "http post is handled on " + getPort();
     }
 
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String getProduct() {
-        return "http get is handled";
+        return "http get is handled on " + getPort();
     }
 
     @PutMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String updateProduct() {
-        return "http put is handled";
+        return "http put is handled on " + getPort();
     }
 
     @DeleteMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String deleteProduct() {
-        return "http delete is handled";
+        return "http delete is handled on " + getPort();
     }
 }
