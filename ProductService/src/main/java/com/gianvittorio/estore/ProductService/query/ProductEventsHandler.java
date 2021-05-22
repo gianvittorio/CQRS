388bf871a60791efcd2df534bf022f2b0b0f0e18
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-05-22 19:21:58 -0300

    done with persisting event data

---
+++ ProductService/src/main/java/com/gianvittorio/estore/ProductService/query/ProductEventsHandler.java
@@ -0,0 +1,25 @@
+package com.gianvittorio.estore.ProductService.query;
+
+import com.gianvittorio.estore.ProductService.core.data.ProductEntity;
+import com.gianvittorio.estore.ProductService.core.data.ProductRepository;
+import com.gianvittorio.estore.ProductService.event.ProductCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductEventsHandler {
+
+    private final ProductRepository productRepository;
+
+    @EventHandler
+    public void on(ProductCreatedEvent event) {
+        ProductEntity productEntity = new ProductEntity();
+
+        BeanUtils.copyProperties(event, productEntity);
+
+        productRepository.save(productEntity);
+    }
+}
