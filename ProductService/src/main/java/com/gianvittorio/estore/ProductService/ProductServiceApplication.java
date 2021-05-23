7f22df3e999872b1325c6f068effec93404827c3
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-05-22 21:06:46 -0300

    done with message dispatch interceptor

--- ProductService/src/main/java/com/gianvittorio/estore/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/gianvittorio/estore/ProductService/ProductServiceApplication.java
@@ -1,8 +1,12 @@
 package com.gianvittorio.estore.ProductService;
 
+import com.gianvittorio.estore.ProductService.command.interceptor.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,8 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext applicationContext, CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(applicationContext.getBean(CreateProductCommandInterceptor.class));
+	}
 }
---
+++ ProductService/src/main/java/com/gianvittorio/estore/ProductService/command/interceptor/CreateProductCommandInterceptor.java
@@ -0,0 +1,36 @@
+package com.gianvittorio.estore.ProductService.command.interceptor;
+
+import com.gianvittorio.estore.ProductService.command.CreateProductCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.messaging.MessageDispatchInterceptor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.BiFunction;
+
+@Component
+@Slf4j
+public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
+    @Override
+    public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(List<? extends CommandMessage<?>> list) {
+        return (index, command) -> {
+
+            log.info("Intercepted command: " + command.getPayloadType());
+
+            if (CreateProductCommandInterceptor.class.equals(command.getPayloadType())) {
+                CreateProductCommand createProductCommand = (CreateProductCommand) command.getPayload();
+
+                if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+                    throw new IllegalArgumentException("Price cannot be less or equal than zero");
+                }
+
+                if (createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+                    throw new IllegalArgumentException("Title cannot be empty");
+                }
+            }
+            return command;
+        };
+    }
+}
