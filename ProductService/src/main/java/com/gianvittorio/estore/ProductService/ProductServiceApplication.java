89770a95a73d95521cc2de5ec7ea976e9e6a90cd
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-05-26 16:04:10 -0300

    done with snapshotting events

--- ProductService/src/main/java/com/gianvittorio/estore/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/gianvittorio/estore/ProductService/ProductServiceApplication.java
@@ -5,11 +5,15 @@ import com.gianvittorio.estore.ProductService.core.errorhandling.ProductServiceE
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -29,5 +33,10 @@ public class ProductServiceApplication {
         configurer.registerListenerInvocationErrorHandler("product-group", conf -> new ProductServiceEventsErrorHandler());
 
 //		configurer.registerListenerInvocationErrorHandler("product-group", conf -> PropagatingErrorHandler.instance());
+    }
+
+    @Bean(name = "productSnapshotTriggerDefinition")
+    public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
     }
 }
